37d695a9f3d95b54f11dbc7a5c5a9d6bd33576cb
tjbo <tomoconnor@helpscout.com>
2020-04-04 17:56:47 -0500

    Change build command

--- rollup.config.js
+++ rollup.config.js
@@ -2,22 +2,15 @@
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
-import replace from 'rollup-plugin-replace'
 import inject from '@rollup/plugin-inject'
-
-const NODE_ENV = process.env.NODE_ENV || 'development'
-const outputFile = NODE_ENV === 'production' ? './lib/prod.js' : './lib/dev.js'
 
 export default {
   input: './src/index.js',
   output: {
-    file: outputFile,
+    file: './build/dist.js',
     format: 'cjs',
   },
   plugins: [
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
-    }),
     babel({
       exclude: 'node_modules/**',
     }),
