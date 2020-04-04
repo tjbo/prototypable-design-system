9984fc4b462feb4671c8ea0b138350ceb725e867
tjbo <tomoconnor@helpscout.com>
2020-04-03 20:24:41 -0500

    Change to cjs

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,69 +1,72 @@
-(function (exports, styled) {
-  'use strict';
+'use strict';
 
-  styled = styled && Object.prototype.hasOwnProperty.call(styled, 'default') ? styled['default'] : styled;
+Object.defineProperty(exports, '__esModule', { value: true });
 
-  const unit = 32;
-  const config = {
-    colors: {
-      dark1: '#000',
-      dark2: '#2b6242',
-      dark3: '#0090e7',
-      dark4: '#454545',
-      light1: '#f5faff',
-      light2: '36,139,204'
-    },
-    domain: 'SomeBiz.com',
-    fonts: {
-      font1: 'PT Serif',
-      font2: 'Roboto',
-      font3: 'PT Serif Caption'
-    },
-    border: '1px solid #eee',
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
-    unit(multiplier) {
-      return multiplier * unit + 'px';
-    },
+var styled = _interopDefault(require('styled-components'));
 
-    layout: {
-      topNavHeight: 2.25 * unit + 'px'
-    },
-    contact: {
-      email: 'biz@yourbiz.com',
-      phone: '999-9999-9999'
-    }
-  };
+const unit = 32;
+const config = {
+  colors: {
+    dark1: '#000',
+    dark2: '#2b6242',
+    dark3: '#0090e7',
+    dark4: '#454545',
+    light1: '#f5faff',
+    light2: '36,139,204'
+  },
+  domain: 'SomeBiz.com',
+  fonts: {
+    font1: 'PT Serif',
+    font2: 'Roboto',
+    font3: 'PT Serif Caption'
+  },
+  border: '1px solid #eee',
 
-  const getBgColor = ({
-    hasBgLightColor,
-    hasBgDarkColor
-  }) => {
-    if (hasBgLightColor) {
-      return `${config.colors.light1}`;
-    } else if (hasBgDarkColor) {
-      return `${config.colors.dark3}`;
-    } else {
-      return '#fff';
-    }
-  };
+  unit(multiplier) {
+    return multiplier * unit + 'px';
+  },
 
-  const getInnerWidth = ({
-    innerWidth
-  }) => {
-    if (innerWidth === 'medium') {
-      return '720px';
-    } else if (innerWidth === 'small') {
-      return '460px';
-    } else {
-      return `${config.unit(33)}`;
-    }
-  };
+  layout: {
+    topNavHeight: 2.25 * unit + 'px'
+  },
+  contact: {
+    email: 'biz@yourbiz.com',
+    phone: '999-9999-9999'
+  }
+};
 
-  const SectionWrapperUI = styled('section')`
+const getBgColor = ({
+  hasBgLightColor,
+  hasBgDarkColor
+}) => {
+  if (hasBgLightColor) {
+    return `${config.colors.light1}`;
+  } else if (hasBgDarkColor) {
+    return `${config.colors.dark3}`;
+  } else {
+    return '#fff';
+  }
+};
+
+const getInnerWidth = ({
+  innerWidth
+}) => {
+  if (innerWidth === 'medium') {
+    return '720px';
+  } else if (innerWidth === 'small') {
+    return '460px';
+  } else {
+    return `${config.unit(33)}`;
+  }
+};
+
+const SectionWrapperUI = styled('section')`
   width: 100%;
   background-color: ${getBgColor};
 `;
-  const SectionUI = styled('div')`
+const SectionUI = styled('div')`
   max-width: ${getInnerWidth};
   margin: 0 auto;
   padding: ${config.unit(1)} 0;
@@ -72,31 +75,27 @@
 }) => textAlign};
 `;
 
-  function section ({
-    children,
-    textAlign = 'left',
-    hasBgDarkColor = false,
-    hasBgLightColor = false,
-    isFullWidth = false,
-    innerWidth = 'big'
-  }) {
-    if (isFullWidth) {
-      return /*#__PURE__*/React.createElement(SectionWrapperUI, {
-        hasBgLightColor: hasBgLightColor
-      }, children);
-    } else {
-      return /*#__PURE__*/React.createElement(SectionWrapperUI, {
-        hasBgLightColor: hasBgLightColor,
-        hasBgDarkColor: hasBgDarkColor
-      }, /*#__PURE__*/React.createElement(SectionUI, {
-        textAlign: textAlign,
-        innerWidth: innerWidth
-      }, children));
-    }
+function section ({
+  children,
+  textAlign = 'left',
+  hasBgDarkColor = false,
+  hasBgLightColor = false,
+  isFullWidth = false,
+  innerWidth = 'big'
+}) {
+  if (isFullWidth) {
+    return /*#__PURE__*/React.createElement(SectionWrapperUI, {
+      hasBgLightColor: hasBgLightColor
+    }, children);
+  } else {
+    return /*#__PURE__*/React.createElement(SectionWrapperUI, {
+      hasBgLightColor: hasBgLightColor,
+      hasBgDarkColor: hasBgDarkColor
+    }, /*#__PURE__*/React.createElement(SectionUI, {
+      textAlign: textAlign,
+      innerWidth: innerWidth
+    }, children));
   }
+}
 
-  exports.Section = section;
-
-  return exports;
-
-}({}, styled));
+exports.Section = section;
