6e1050dd418ff58394246de16c68e0137d5528be
tjbo <tomoconnor@helpscout.com>
2020-04-17 16:41:13 -0500

    Fix window

--- src/components/body/modal.js
+++ src/components/body/modal.js
@@ -1,16 +1,22 @@
 export const onShowModal = () => {
-  const scrollY = document.documentElement.style.getPropertyValue('--scroll-y')
-  const body = document.body
-  body.style.position = 'fixed'
-  body.style.top = `-${scrollY}`
+  if (typeof window !== 'undefined') {
+    const scrollY = document.documentElement.style.getPropertyValue(
+      '--scroll-y',
+    )
+    const body = document.body
+    body.style.position = 'fixed'
+    body.style.top = `-${scrollY}`
+  }
 }
 
 export const onHideModal = () => {
-  const body = document.body
-  const scrollY = body.style.top
-  body.style.position = ''
-  body.style.top = ''
-  window.scrollTo(0, parseInt(scrollY || '0') * -1)
+  if (typeof window !== 'undefined') {
+    const body = document.body
+    const scrollY = body.style.top
+    body.style.position = ''
+    body.style.top = ''
+    window.scrollTo(0, parseInt(scrollY || '0') * -1)
+  }
 }
 
 if (typeof window !== 'undefined') {
