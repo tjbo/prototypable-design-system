1ab5229938cd46f04a66655f8b07dc61c7403a70
tjbo <tomoconnor@helpscout.com>
2020-04-22 20:45:12 -0500

    Add clasue for window

--- src/components/head/breakPoints.js
+++ src/components/head/breakPoints.js
@@ -2,25 +2,29 @@ const callbacks = []
 let currentBreakpoint
 
 function refreshValue() {
-  currentBreakpoint = window
-    .getComputedStyle(document.querySelector('body'), ':before')
-    .getPropertyValue('content')
-    .replace(/\"/g, '')
+  if (typeof window !== 'undefined') {
+    currentBreakpoint = window
+      .getComputedStyle(document.querySelector('body'), ':before')
+      .getPropertyValue('content')
+      .replace(/\"/g, '')
 
-  for (let callback in callbacks) {
-    callbacks[callback](currentBreakpoint)
+    for (let callback in callbacks) {
+      callbacks[callback](currentBreakpoint)
+    }
   }
 }
 
 window.addEventListener('resize', refreshValue)
 
 export function addListener(callback) {
-  callbacks.push(callback)
+  if (typeof window !== 'undefined') {
+    callbacks.push(callback)
 
-  currentBreakpoint = window
-    .getComputedStyle(document.querySelector('body'), ':before')
-    .getPropertyValue('content')
-    .replace(/\"/g, '')
+    currentBreakpoint = window
+      .getComputedStyle(document.querySelector('body'), ':before')
+      .getPropertyValue('content')
+      .replace(/\"/g, '')
 
-  return currentBreakpoint
+    return currentBreakpoint
+  }
 }
