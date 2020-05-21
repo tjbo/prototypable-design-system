a6c9ddc46e767fdd4191aca7651434558d51f6fd
tjbo <tomoconnor@helpscout.com>
2020-05-21 14:03:07 -0500

    REmvoe links

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -3,36 +3,6 @@ import GlobalStyles from './styles.css'
 export default function () {
   return (
     <React.Fragment>
-      <link
-        rel="preload"
-        as="style"
-        href="/fonts/roboto-v20-latin-300.woff"
-        type="font/woff"
-        crossOrigin="anonymous"
-      />
-      <link
-        rel="preload"
-        as="style"
-        href="/fonts/roboto-v20-latin-300.woff2"
-        type="font/woff2"
-        crossOrigin="anonymous"
-      />
-
-      <link
-        rel="preload"
-        as="style"
-        href="/fonts/roboto-v20-latin-700.woff"
-        type="font/woff"
-        crossOrigin="anonymous"
-      />
-      <link
-        rel="preload"
-        as="style"
-        href="/fonts/roboto-v20-latin-700.woff2"
-        type="font/woff2"
-        crossOrigin="anonymous"
-      />
-
       <GlobalStyles />
     </React.Fragment>
   )
