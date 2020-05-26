4c3c70a080466021197a4884bb9e4b892a1c2ee0
tjbo <tomoconnor@helpscout.com>
2020-05-26 15:21:45 -0500

    Add icon

---
+++ src/components/icon/icon.css.js
@@ -0,0 +1,3 @@
+export const IconUI = styled('img')`
+  max-width: 30px;
+`
---
+++ src/components/icon/icon.js
@@ -0,0 +1,5 @@
+import { IconUI } from './icon.css'
+
+export default function Icon({ alt, src }) {
+  return <IconUI src={src} alt={alt} />
+}
