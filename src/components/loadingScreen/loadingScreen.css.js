d2213e43a3dbfa77bde25cfef9da2ff83b4b1b4e
tjbo <tomoconnor@helpscout.com>
2020-07-26 14:51:50 -0500

    Remove unused components

--- src/components/loadingScreen/loadingScreen.css.js
+++
@@ -1,9 +0,0 @@
-import theme from '../../theme'
-
-export const LoadingScreenContainerUI = styled('div')`
-  align-items: center;
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-`
--- src/components/loadingScreen/loadingScreen.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import LoadingScreen from './'
-
-export default {
-  title: 'Loading Screen',
-  component: LoadingScreen,
-}
-
-export const Default = () => <LoadingScreen />
